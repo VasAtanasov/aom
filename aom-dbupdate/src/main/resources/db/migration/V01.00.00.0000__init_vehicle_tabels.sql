70088a8069bdc7f6a2d0005031556626e25589a0
VasAtanasov <vas.atanasov@gmail.com>
2023-02-24 11:09:46 +0200

    Define initial database scripts for creating database tables.#6

---
+++ aom-dbupdate/src/main/resources/db/migration/V01.00.00.0000__init_vehicle_tabels.sql
@@ -0,0 +1,91 @@
+CREATE TABLE IF NOT EXISTS `maker`
+(
+    `id`   int NOT NULL AUTO_INCREMENT,
+    `name` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `INDEX_MAKER_NAME` (`name`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `model`
+(
+    `id`       int NOT NULL AUTO_INCREMENT,
+    `name`     varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `maker_id` int NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `INDEX_MODEL_NAME` (`name`),
+    KEY `FK_MODEL_TO_MAKE_ID` (`maker_id`),
+    CONSTRAINT `FK_MODEL_TO_MAKE_ID` FOREIGN KEY (`maker_id`) REFERENCES `maker` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `model_year`
+(
+    `id`       int NOT NULL AUTO_INCREMENT,
+    `year`     int NOT NULL,
+    `model_id` int NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `INDEX_MODEL_YEAR_YEAR` (`year`),
+    KEY `FK_CARS_TO_MODELS_ID` (`model_id`),
+    CONSTRAINT `FK_CARS_TO_MODELS_ID` FOREIGN KEY (`model_id`) REFERENCES `model` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `trim`
+(
+    `id`     int                                     NOT NULL AUTO_INCREMENT,
+    `trim`   varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `car_id` int                                     NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `INDEX_TRIM_NAME` (`trim`),
+    KEY `FK_TRIMS_TO_CARS_ID` (`car_id`),
+    CONSTRAINT `FK_TRIMS_TO_CARS_ID` FOREIGN KEY (`car_id`) REFERENCES `model_year` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `engine`
+(
+    `id`          int(11) NOT NULL AUTO_INCREMENT,
+    `is_standard` bit(1)                                  DEFAULT NULL,
+    `name`        varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `trim_id`     int(11) NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `INDEX_ENGINE_NAME` (`name`),
+    KEY `INDEX_ENGINE_IS_STANDARD` (`is_standard`),
+    KEY `FK_ENGINES_TO_TRIMS_ID` (`trim_id`),
+    CONSTRAINT `FK_ENGINES_TO_TRIMS_ID` FOREIGN KEY (`trim_id`) REFERENCES `trim` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `transmission`
+(
+    `id`      int NOT NULL AUTO_INCREMENT,
+    `name`    varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `trim_id` int NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `INDEX_TRANSMISSION_NAME` (`name`),
+    KEY `FK_TRANSMISSIONS_TO_TRIMS_ID` (`trim_id`),
+    CONSTRAINT `FK_TRANSMISSIONS_TO_TRIMS_ID` FOREIGN KEY (`trim_id`) REFERENCES `trim` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `option`
+(
+    `id`        int NOT NULL AUTO_INCREMENT,
+    `active`    bit                                     DEFAULT NULL,
+    `important` bit                                     DEFAULT NULL,
+    `name`      varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `trim_id`   int NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `INDEX_OPTION_NAME` (`name`),
+    KEY `FK_OPTIONS_TO_TRIMS_ID` (`trim_id`),
+    CONSTRAINT `FK_OPTIONS_TO_TRIMS_ID` FOREIGN KEY (`trim_id`) REFERENCES `trim` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
