59e9b29277152bc447e314c7eb48ee068c3e0fe7
VasAtanasov <vas.atanasov@gmail.com>
2023-03-28 16:25:29 +0300

    Reorganise the modules and package structure #23

--- aom-dao/src/main/java/com/github/vaatech/core/aom/specification/ModelSpecifications.java
+++ aom-dao/src/main/java/com/github/vaatech/core/aom/specification/ModelSpecifications.java
@@ -1,16 +1,24 @@
 package com.github.vaatech.core.aom.specification;
 
+import com.github.vaatech.aom.core.model.vehicle.Model;
+import com.github.vaatech.aom.core.model.vehicle.ModelYear;
+import com.github.vaatech.aom.core.model.vehicle.ModelYear_;
+import com.github.vaatech.aom.core.model.vehicle.Model_;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import org.springframework.data.jpa.domain.Specification;
+
 public class ModelSpecifications {
 
-//  public static Specification<Model> carsForModelId(String id) {
-//    return (root, query, cb) -> {
-//      Join<Model, Car> join = root.join(Model_.cars, JoinType.LEFT);
-//      query.distinct(true);
-//      return cb.equal(join.get(Car_.model), id);
-//    };
-//  }
+  public static Specification<Model> carsForModelId(String id) {
+    return (root, query, cb) -> {
+      Join<Model, ModelYear> join = root.join(Model_.modelYears, JoinType.LEFT);
+      query.distinct(true);
+      return cb.equal(join.get(ModelYear_.model), id);
+    };
+  }
 
-//  public static Specification<Model> byId(String id) {
-//    return (root, query, cb) -> cb.equal(root.get(Model_.id), id);
-//  }
+  public static Specification<Model> byId(String id) {
+    return (root, query, cb) -> cb.equal(root.get(Model_.id), id);
+  }
 }
